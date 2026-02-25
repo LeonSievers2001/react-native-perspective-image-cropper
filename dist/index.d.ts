1f1339caefb08942fa096835c28f26e2d96a128a
LeonSievers2001 <sievers@netzhirsch.de>
2026-02-25 13:09:30 +0100

    16KB Page size support

---
+++ dist/index.d.ts
@@ -0,0 +1,4 @@
+import React from 'react';
+import type { Props, Ref } from './types';
+declare const CustomCrop: React.ForwardRefExoticComponent<Props & React.RefAttributes<Ref>>;
+export { CustomCrop };
